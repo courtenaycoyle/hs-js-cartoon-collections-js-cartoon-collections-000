0d9c8185284c35598c68aef6f48963fbf077ab2a
Courtenay Coyle <courtenay.coyle@gmail.com>
2019-03-15 17:38:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,59 @@
+
 function dwarfRollCall(dwarves) {
+  
+  var dwarvesString = "";
+  var empty = [];
+  for(var i = 0; i < dwarves.length; i++) {
+  
+
+     dwarvesString +=`${i + 1}. ${dwarves[i]} `;
+    
+}
+
+return  dwarvesString;
+
+
+
+ 
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var empty = [];
+  var string = "";
+  var upperString = "";
+  for(var i = 0; i < planeteerCalls.length; i++) {
+  string = `${planeteerCalls[i]}!`;
+  upperString = string.toUpperCase();
+
+
+  empty.push(upperString);
+  
+  
+}
+  return empty;
 }
 
 function longPlaneteerCalls(words) {
+  var i = 0;
+  if (words[i].length > 4) {
+  
+    return true;
+  }else {
+    return false;
+  }
+  
 }
 
 function findTheCheese (foods) {
+
+  var cheese = "cheddar";
+    for (var i = 0; i < foods.length; i++) {
+        if (foods[i] === cheese) {
+            return "cheddar";
+        }
+    }
+    return "no cheese!";
+
+
+
 }
